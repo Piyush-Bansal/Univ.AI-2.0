7b03b94bc0e031579e2a9802c4c3aa0c9563917e
Piyush-Bansal <39044544+Piyush-Bansal@users.noreply.github.com>
2022-09-04 21:19:19 +0530

    test 2

--- animation/index.js
+++ animation/index.js
@@ -7,10 +7,16 @@ openMenu
     opacity: 0,
     x: "-100%",
   })
+  .to(".nav__close--wrapper", {
+    opacity: 0,
+  })
   .to(".nav__bg", {
     opacity: 1,
   })
   .to(".sidemenu", {
-    opacity: 0,
+    opacity: 1,
     x: 0,
+  })
+  .to(".nav__close--wrapper", {
+    opacity: 1,
   });
