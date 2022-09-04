b5ae3c4ffdc2a8c62ede97b8f6b7dad2fbc97b52
Piyush-Bansal <39044544+Piyush-Bansal@users.noreply.github.com>
2022-09-04 21:21:53 +0530

    test 3

--- animation/index.js
+++ animation/index.js
@@ -1,14 +1,11 @@
 let openMenu = gsap.timeline();
 openMenu
-  .from(".nav__bg", {
+  .from([".nav__bg", ".nav__close--wrapper"], {
     opacity: 0,
   })
   .from(".sidemenu", {
     opacity: 0,
     x: "-100%",
-  })
-  .to(".nav__close--wrapper", {
-    opacity: 0,
   })
   .to(".nav__bg", {
     opacity: 1,
