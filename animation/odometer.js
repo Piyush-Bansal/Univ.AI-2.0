1857f645a6331a25267bbd6ff0edfd7e73637766
Piyush-Bansal <39044544+Piyush-Bansal@users.noreply.github.com>
2022-09-14 23:05:09 +0530

    small changes

--- animation/odometer.js
+++ animation/odometer.js
@@ -2,7 +2,7 @@ $(".starter-odometer").each(function () {
   var startValue = "1" + "0" + "1";
   let content = this.textContent;
 
-  var endValue = content;
+  var endValue = "1" + content + "1";
   var odometer = new Odometer({
     el: this,
     value: startValue,
