9054e6ce86d342325bd371a64447f22a1e75b6c6
Piyush-Bansal <39044544+Piyush-Bansal@users.noreply.github.com>
2022-09-15 00:02:17 +0530

    tragger adjustment

--- animation/odometer.js
+++ animation/odometer.js
@@ -12,7 +12,7 @@ $(".starter-odometer").each(function () {
   odometer.render(startValue);
   ScrollTrigger.create({
     trigger: this,
-    start: "top 85%",
+    start: "center 90%",
     onEnter: () => {
       odometer.update(endValue);
     },
