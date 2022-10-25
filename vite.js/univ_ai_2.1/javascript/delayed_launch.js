7b95b325571610bdb10c297e541b3d0b1537d2c1
Piyush-Bansal <39044544+Piyush-Bansal@users.noreply.github.com>
2022-10-26 01:45:53 +0530

    changes

--- vite.js/univ_ai_2.1/javascript/delayed_launch.js
+++ vite.js/univ_ai_2.1/javascript/delayed_launch.js
@@ -1,15 +1,19 @@
-setTimeout(() => {
-  let source = [
-    "https://cdn.jsdelivr.net/gh/Piyush-Bansal/Univ.AI-2.0@84897ed6c41e0cd61f26a06bcf090dcb8ba070f2/vite.js/univ_ai_2.1/javascript/variable%20aspect%20ratio.min.js",
-    "https://cdn.jsdelivr.net/gh/Piyush-Bansal/Univ.AI-2.0@84897ed6c41e0cd61f26a06bcf090dcb8ba070f2/vite.js/univ_ai_2.1/javascript/reduce%20columns.min.js",
-    "https://cdn.jsdelivr.net/gh/Piyush-Bansal/Univ.AI-2.0@84897ed6c41e0cd61f26a06bcf090dcb8ba070f2/vite.js/univ_ai_2.1/javascript/modal.min.js",
-    "https://cdn.jsdelivr.net/gh/Piyush-Bansal/Univ.AI-2.0@84897ed6c41e0cd61f26a06bcf090dcb8ba070f2/vite.js/univ_ai_2.1/javascript/marquee.min.js",
-    "https://cdn.jsdelivr.net/gh/Piyush-Bansal/Univ.AI-2.0@84897ed6c41e0cd61f26a06bcf090dcb8ba070f2/vite.js/univ_ai_2.1/javascript/form.min.js",
-    "https://cdn.jsdelivr.net/gh/Piyush-Bansal/Univ.AI-2.0@84897ed6c41e0cd61f26a06bcf090dcb8ba070f2/vite.js/univ_ai_2.1/javascript/footer%20year.min.js",
-  ];
-
-  let script = document.createElement("script");
-  script.setAttribute("type", "text/javascript");
-  script.setAttribute("src", "jquery.min.js");
-  document.head.appendChild(script);
-}, "2000");
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    let head = document.getElementsByTagName("head")[0];
+    let sources = [
+      "https://cdn.jsdelivr.net/gh/Piyush-Bansal/Univ.AI-2.0@227f0e20d9f0e6bd252e421a5e91ad5960af220d/vite.js/univ_ai_2.1/javascript/marquee.min.js",
+      "https://cdn.jsdelivr.net/gh/Piyush-Bansal/Univ.AI-2.0@227f0e20d9f0e6bd252e421a5e91ad5960af220d/vite.js/univ_ai_2.1/javascript/variable%20aspect%20ratio.min.js",
+      "https://cdn.jsdelivr.net/gh/Piyush-Bansal/Univ.AI-2.0@227f0e20d9f0e6bd252e421a5e91ad5960af220d/vite.js/univ_ai_2.1/javascript/reduce%20columns.min.js",
+      "https://cdn.jsdelivr.net/gh/Piyush-Bansal/Univ.AI-2.0@227f0e20d9f0e6bd252e421a5e91ad5960af220d/vite.js/univ_ai_2.1/javascript/modal.min.js",
+      "https://cdn.jsdelivr.net/gh/Piyush-Bansal/Univ.AI-2.0@227f0e20d9f0e6bd252e421a5e91ad5960af220d/vite.js/univ_ai_2.1/javascript/form.min.js",
+      "https://cdn.jsdelivr.net/gh/Piyush-Bansal/Univ.AI-2.0@227f0e20d9f0e6bd252e421a5e91ad5960af220d/vite.js/univ_ai_2.1/javascript/footer%20year.min.js",
+    ];
+    sources.forEach((source) => {
+      let script = document.createElement("script");
+      script.setAttribute("type", "text/javascript");
+      script.setAttribute("src", source);
+      document.head.appendChild(script);
+    });
+  }, "5000");
+});
