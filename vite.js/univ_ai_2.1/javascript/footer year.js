be6bf5d0a3f6f297fce4d0b27468e19449a8aac2
Piyush-Bansal <39044544+Piyush-Bansal@users.noreply.github.com>
2022-10-26 01:46:50 +0530

    changes

--- vite.js/univ_ai_2.1/javascript/footer year.js
+++ vite.js/univ_ai_2.1/javascript/footer year.js
@@ -1,5 +1,3 @@
-window.addEventListener("load", () => {
-  //add year on footer
-  const footerYear = document.querySelector(".year");
-  footerYear.innerHTML = new Date().getFullYear();
-});
+//add year on footer
+const footerYear = document.querySelector(".year");
+footerYear.innerHTML = new Date().getFullYear();
