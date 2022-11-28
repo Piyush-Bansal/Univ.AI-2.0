acfe78ce054cbeb9236ce93f83df0d64f5686efa
Piyush-Bansal <39044544+Piyush-Bansal@users.noreply.github.com>
2022-11-28 17:16:36 +0530

    changes

--- vite.js/univ_ai_2.1/javascript/delayed_launch.js
+++ vite.js/univ_ai_2.1/javascript/delayed_launch.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", () => {
   setTimeout(() => {
     let head = document.getElementsByTagName("head")[0];
-    let commitId = "1704541382260283e13cd6a828beef61e7d95f4a";
+    let commitId = "651e17f52ed13395fc0901a984374a873de6b9bf";
     let filePath = `https://cdn.jsdelivr.net/gh/Piyush-Bansal/Univ.AI-2.0@${commitId}/vite.js/univ_ai_2.1/javascript/`;
     let sources = [
       "horizontal_text.min.js",
